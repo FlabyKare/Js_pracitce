806cc28f4d6b45b03494313977afd147e089a4b9
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-08-07 12:41:42 +0300

    lesson_3_finished

--- lesson_3/lesson_3.js
+++ lesson_3/lesson_3.js
@@ -1,6 +1,20 @@
-const block = document.querySelector('.block');
-const btn = document.querySelector('.btn');
-const body = document.querySelector('body');
-const block_add = document.querySelector('.block_add');
+const btn = document.getElementById('js-btn'),
+         nickName = document.getElementById('js-name'),
+         password = document.getElementById('js-password')
+let accountsInfo = [
+    {name: ''},
+    {password: ''}
+]
+btn.addEventListener('click', () => {
+    if(nickName.value != '' && password.value != ''){
+        window.open('account.html', '_blank');
+        accountsInfo.name = nickName.value 
+        accountsInfo.password = password.value 
+        // console.log(accountsInfo)
+        btn.innerHTML = "Авторизован: " + accountsInfo.name + `<br style="margin-bottom: 2%;">` + "С паролем: " + password.value
+    } else{
+        alert('Вы не являетесь владельцем данного аккаунта')
+    }
+})
 
 
